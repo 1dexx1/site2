6135b0afd5f0a1a222d346d46d5ba91db288dc3c
1dexx1 <artemzubchenko7@gmail.com>
2025-02-06 17:52:47 +0200

    убегать

--- src/consts.ts
+++ src/consts.ts
@@ -18,17 +18,18 @@ export const SITE: Site = {
     'тута моя биография вся.',
   EMAIL: '',
   NUM_POSTS_ON_HOMEPAGE: 2,
-  POSTS_PER_PAGE: 3,
+  POSTS_PER_PAGE: 5,
   SITEURL: 'https://site2-weld-eight.vercel.app',
 }
 
 export const NAV_LINKS: Link[] = [
+  { href: '/', label: 'главная'},
   { href: '/about', label: 'о себе' },
   { href: '/blog', label: 'блог' },
   { href: '/tags', label: 'теги' },
   { href: '/work', label: 'проекты'},
   { href: '/authors', label: 'авторы' },
-  { href: '/members', label: 'учасники'}
+  { href: '/members', label: 'учасники'},
 ]
 
 export const SOCIAL_LINKS: Link[] = [
--- src/content/members/gtgaster.md
+++ src/content/members/gtgaster.md
@@ -2,5 +2,5 @@
 name: 'gtgaster'
 pronouns: 'он/его'
 avatar: 'https://cdn.discordapp.com/avatars/999589862158114848/1090dfff77cba9c19ddd0ebe0409ed5d.webp?size=2048'
-bio: 'просто гаст'
+bio: 'фурри, просто фурри'
 ---
