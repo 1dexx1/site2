00ecd51d25a64d3b6b0c5e5ef204ad5f20e3e2fd
1dexx1 <artemzubchenko7@gmail.com>
2025-03-07 22:55:03 +0200

    фыввфы

--- src/consts.ts
+++ src/consts.ts
@@ -31,6 +31,7 @@ export const NAV_LINKS: Link[] = [
   { href: '/authors', label: 'авторы' },
   { href: '/members', label: 'учасники'},
   { href: '/links', label: 'ссылки'},
+  { href: '/dni', label: 'dni'},
 ]
 
 export const SOCIAL_LINKS: Link[] = [
